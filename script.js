cbd9d625278223b7efcdf5034ab904bd18207e65
Ildar <aslyamov1997@icloud.com>
2020-03-05 16:42:16 +0500

    Исправил ошибки

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+    let nav = document.getElementById("nav");
+    nav.classList.toggle('active');
+
+    let toggle = document.getElementById("toggle");
+    toggle.classList.toggle('active');
+}
